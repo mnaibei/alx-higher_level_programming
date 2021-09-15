cf985d3b9075321dbe6f1166a978678f77799d0a
Mucha <muchajulius@gmail.com>
2021-09-15 20:21:49 +0300

    data_Structures

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,13 @@
+!/usr/bin/python3
+
+
+def print_sorted_dictionary(my_dict):
+
+    keys = []
+    if my_dict:
+        for k, i in my_dict.items():
+            keys.append(k)
+
+        keys.sort()
+        for k in keys:
+            print("{}: {}".format(k, my_dict[k]))
