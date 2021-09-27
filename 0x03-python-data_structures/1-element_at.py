1f85900de5cdfe8db2d3dba0126dda18e2a8843e
Mucha <muchajulius@gmail.com>
2021-09-27 19:29:55 +0300

    .

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (idx < 0 or idx > len(my_list) - 1):
-        return (None)
-    return (my_list[idx])
-
-if __name__ == "__main__":
-    element_at(0, 0)
+    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
