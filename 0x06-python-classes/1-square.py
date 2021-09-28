707caf1870ae72b88deee5704ed27d3f40934764
Mucha <muchajulius@gmail.com>
2021-09-28 20:54:18 +0300

    .

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""Define class square"""
+# 1-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
 
 
 class Square:
-    def __init__(self, size):
-        """Initialization of instance attributes
+    """Represent a square."""
 
+    def __init__(self, size):
+        """Initialize a new Square.
         Args:
-            size (int): The size of the square
+            size (int): The size of the new square.
         """
         self.__size = size
