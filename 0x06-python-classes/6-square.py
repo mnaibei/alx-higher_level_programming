1f3ffd9b9812573845c4d7b3749501f1ad40545d
Mucha <muchajulius@gmail.com>
2021-09-28 20:52:34 +0300

    .

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,70 +1,60 @@
 #!/usr/bin/python3
-"""Define a class square"""
+# 6-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
 
 
-class Square():
-    '''
-        represent a Square
-    '''
+class Square:
+    """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        '''Initialization of instance attributes
-            Args:
-            size (int): Zero or positve number.
-        '''
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.size = size
         self.position = position
 
-    def area(self):
-        '''Calculates the area
-
-            Return: The current square area.
-        '''
-        return self.__size * self.__size
-
     @property
     def size(self):
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        '''Updating the private attributes
-            Args:
-            value (int): Zero or positve number.
-        '''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
     def position(self):
-        return self.__position
+        """Get/set the current position of the square."""
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        '''Updating the private attributes
-            Args:
-            value (int): tuple of two positve numbers.
-        '''
-        if isinstance(value, tuple) and len(value) == 2:
-            if isinstance(value[0], int) and isinstance(value[1], int):
-                if value[0] >= 0 and value[1] >= 0:
-                    self.__position = value
-                    return
-                raise TypeError("position must be a tuple of 2 positive integers")
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
-            def my_print(self):
-                '''
-                prints in stdout the square with the character # or a new line
-                is size is zero.
-                '''
-                if self.__size == 0:
-                    print()
-                    return
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
-                for line in range(self.__position[1]):
-                    print()
-                    for col in range(self.__size):
-                        print("{}{}".format(" " * self.__position[0], "#" * self.__size))
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
