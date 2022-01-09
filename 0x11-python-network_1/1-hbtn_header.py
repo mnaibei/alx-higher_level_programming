5867f0da80e3bf4b36161ab6536b5eaad17202d6
Mucha <muchajulius@gmail.com>
2022-01-09 21:34:47 +0300

    .

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# Python script that takes in a URL sends a request to the URL
-import sys
-import urllib.request
+""" Takes in a URL, sends a request to the URL """
+
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as res:
-        """ print(res.info()['X-Request-Id'])"""
-        head = res.headers.get('X-Request-Id')
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        head = response.headers.get('X-Request-Id')
         print(head)
